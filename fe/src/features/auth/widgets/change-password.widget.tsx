d6ae2ac3d681fa346aff3026b7fcff5ac619faa8
khiemnd777 <khiemnd777@gmail.com>
2026-04-24 14:11:34 +0700

    fix: spacer

--- fe/src/features/auth/widgets/change-password.widget.tsx
+++ fe/src/features/auth/widgets/change-password.widget.tsx
@@ -5,17 +5,21 @@ import { AutoForm } from "@root/core/form/auto-form";
 import { SafeButton } from "@shared/components/button/safe-button";
 import { registerSlot } from "@root/core/module/registry";
 import ChangeCircleOutlinedIcon from '@mui/icons-material/ChangeCircleOutlined';
+import { Spacer } from "@root/shared/components/ui/spacer";
 
 function ChangePasswordWidget() {
   const formAccountChangePasswordRef = React.useRef<AutoFormRef>(null);
   return (
-    <SectionCard title={"Đổi mật khẩu"} extra={
-      <SafeButton variant="contained" startIcon={<ChangeCircleOutlinedIcon />} onClick={() => formAccountChangePasswordRef.current?.submit()}>
-        Đổi
-      </SafeButton>
-    }>
-      <AutoForm name="account-change-password" ref={formAccountChangePasswordRef} />
-    </SectionCard>
+    <>
+      <SectionCard title={"Đổi mật khẩu"} extra={
+        <SafeButton variant="contained" startIcon={<ChangeCircleOutlinedIcon />} onClick={() => formAccountChangePasswordRef.current?.submit()}>
+          Đổi
+        </SafeButton>
+      }>
+        <AutoForm name="account-change-password" ref={formAccountChangePasswordRef} />
+      </SectionCard>
+      <Spacer />
+    </>
   );
 }
 
